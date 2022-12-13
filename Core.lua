76aee2ba96d8d256280a93082f987daf1a6f0f5b
muleyo <leifm1996@gmail.com>
2022-12-13 10:24:28 +0100

    Fixed an issue which made the frames break when SortGroup or similar AddOns were active

--- Core.lua
+++ Core.lua
@@ -1,27 +1,21 @@
 PFR_Core = PFR:NewModule("PFR_Core")
 
 function PFR_Core:OnEnable()
-    function PFR:Update()
-        for i = 1, 5 do
-            _G["CompactPartyFrameMember" .. i]:SetWidth(PFR_DB.width)
-            _G["CompactPartyFrameMember" .. i]:SetHeight(PFR_DB.height)
-            _G["CompactPartyFrameMember" .. i .. "StatusText"]:ClearAllPoints()
-            _G["CompactPartyFrameMember" .. i .. "StatusText"]:SetPoint("CENTER", _G["CompactPartyFrameMember" .. i], "CENTER")
-            _G["CompactPartyFrameMember" .. i .. "CenterStatusIcon"]:ClearAllPoints()
-            _G["CompactPartyFrameMember" .. i .. "CenterStatusIcon"]:SetPoint("CENTER", _G["CompactPartyFrameMember" .. i], "CENTER")
+    function PFR:Update(self)
+        if self:IsForbidden() then return end
+		local name = self:GetName()
+
+        if name and name:match("CompactPartyFrame") then
+            self:SetWidth(PFR_DB.width)
+            self:SetHeight(PFR_DB.height)
+            self.statusText:ClearAllPoints()
+            self.statusText:SetPoint("CENTER", self, "CENTER")
+            self.centerStatusIcon:ClearAllPoints()
+            self.centerStatusIcon:SetPoint("CENTER", self, "CENTER")
         end
     end
 
-    -- Register Events
-    PFR:RegisterEvent("ADDON_LOADED", "Update")
-    PFR:RegisterEvent("PLAYER_LOGIN", "Update")
-    PFR:RegisterEvent("VARIABLES_LOADED", "Update")
-    PFR:RegisterEvent("PLAYER_ENTERING_WORLD", "Update")
-    PFR:RegisterEvent("GROUP_ROSTER_UPDATE", "Update")
-    PFR:RegisterEvent("COMPACT_UNIT_FRAME_PROFILES_LOADED", "Update")
-
-    -- Call function on exiting EditMode
-    hooksecurefunc(C_EditMode, "OnEditModeExit", function()
-        PFR:Update()
+    hooksecurefunc("CompactUnitFrame_UpdateAll", function(self)
+        PFR:Update(self)
     end)
 end
